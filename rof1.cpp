77c63d6999df7cc6ccaabb4ecccbe8055b0d1786
amitstar791 <amitstar791@gmail.com>
2025-08-17 13:51:08 +0000

    simple program

--- rof1.cpp
+++ rof1.cpp
@@ -1,1 +1,11 @@
-
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Hello World ";
+    // add a loop to print "from rof1.cpp" 5 times
+    for (int i = 0; i < 5; ++i) {
+        cout << "from rof1.cpp " << endl;
+    }
+    cout << endl;       
+    return 0;
+}
--- rof2.cpp
+++ rof2.cpp
@@ -1,1 +1,6 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello World from rof2.cpp" << endl;
+    return 0;
+}
